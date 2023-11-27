1ef37178e31c4403ca590e5223b17b7602b0c72d
irensmile <curkan.ira@gmail.com>
2023-11-27 11:22:05 -0500

    наслідування стилів

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -3,7 +3,7 @@ import css from './FriendListItem.module.css'
 
 export const FriendListItem = ({friend}) => (
 <li className={css.item}>
-        <span className={[css.status, friend.isOnline ? css.online : css.offline].join(' ')}></span>
+        <span className={friend.isOnline ? css.online : css.offline}></span>
         <img className={css.avatar} src={friend.avatar} alt="User avatar" width="48" />
         <p className={css.name}>{friend.name }</p>
 </li>
